a5f21aadba4b5bdace78eff018224c91b15492a8
hiroshimaeasyryo <hiroshimaeasyryo@users.noreply.github.com>
2023-10-12 23:34:52 +0000

    Deploying to gh-pages from @ hiroshimaeasyryo/pbr_pjt@e00580eb4741a565b39aee7930b682b23d635761 🚀

--- docs/script.js
+++
@@ -1,8 +0,0 @@
-// script.js
-window.onload = function() {
-    var navbarHeight = document.querySelector('.navbar').offsetHeight;
-    var footerHeight = document.querySelector('footer').offsetHeight;
-    var windowHeight = window.innerHeight;
-    var graphHeight = windowHeight - navbarHeight - footerHeight;
-    document.querySelector('#graph').style.height = graphHeight + 'px';
-};
