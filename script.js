34499d87a948f4e337fee5c0e7ae54939c4ff8ac
hiroshimaeasyryo <hiroshimaeasyryo@users.noreply.github.com>
2025-08-10 07:26:27 +0000

    Deploying to gh-pages from @ hiroshimaeasyryo/pbr_pjt@176fa89f74e2a2ec394ee72e9f471a8731ee0794 🚀

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 // script.js
 window.onload = function() {
-    var navbarHeight = document.querySelector('.navbar').offsetHeight;
-    var footerHeight = document.querySelector('footer').offsetHeight;
+    var navbar = document.querySelector('.navbar');
+    var footer = document.querySelector('footer');
+    var plotDiv = document.querySelector('.plotly-graph-div');
+
+    if (!plotDiv) return;
+
+    var navbarHeight = navbar ? navbar.offsetHeight : 0;
+    var footerHeight = footer ? footer.offsetHeight : 0;
     var windowHeight = window.innerHeight;
-    var graphHeight = windowHeight - navbarHeight - footerHeight;
-    document.querySelector('#graph').style.height = graphHeight + 'px';
+    var graphHeight = Math.max(200, windowHeight - navbarHeight - footerHeight);
+    plotDiv.style.height = graphHeight + 'px';
 };
